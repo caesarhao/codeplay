1575fd38e9b60baf9b476d1a3ea18e49e3a116a6
Jungle Schmidt <caesarhao@126.com>
2026-02-19 14:31:31 +0100

    Update first.py

--- tkinter/first.py
+++ tkinter/first.py
@@ -1,7 +1,23 @@
 import sys
 import tkinter as tk
+from tkinter import messagebox
 print (f"Python version : {sys.version}")
+def on_button_click():
+  user_name = entry.get()
+  if user_name:
+    messagebox.showinfo("Info", f"Welcome {user_name}!")
+  else:
+    messagebox.showwarning("Warning", "Please input user name")
+def on_exit():
+  if messsagebox.askquestion("Question", "Are you sure to exit?") == 'yes':
+    root.quit()
+
 root = tk.Tk()
 root.title("My first Tkinter Example")
-root.geometry("400x300")
+root.geometry("450x350")
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+x = (screen_width // 2) - (450 // 2)
+y = (screen_height // 2) - (350 // 2)
+root.geometry(f"450x350+{x}+{y}")
 root.mainloop()
